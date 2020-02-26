01a28f9b954759ad2de3ff9397c59c4efa868d39
takemosnter8 <takemonster8@naver.com>
2020-02-26 17:16:22 +0900

    Updates

---
+++ precache-manifest.139aeaca1034e9fc87517319ff8a66df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339212e130fecbc8ca25596129a57326",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "639acad5ce1338cecc2f",
+    "url": "/movie_app_2019/static/css/main.7abe7edb.chunk.css"
+  },
+  {
+    "revision": "70f0c6002f4486a8ba63",
+    "url": "/movie_app_2019/static/js/2.2d8a139a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2019/static/js/2.2d8a139a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639acad5ce1338cecc2f",
+    "url": "/movie_app_2019/static/js/main.1a1d80d1.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
